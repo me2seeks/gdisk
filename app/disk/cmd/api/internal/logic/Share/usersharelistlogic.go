46f674a3465f07f34fab473dd4e705bbc39e1a76
LL <z3653934@163.com>
2022-11-27 16:22:23 +0800

    fix:  sharelist

--- app/disk/cmd/api/internal/logic/Share/usersharelistlogic.go
+++ app/disk/cmd/api/internal/logic/Share/usersharelistlogic.go
@@ -24,7 +24,27 @@ func NewUserShareListLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Use
 }
 
 func (l *UserShareListLogic) UserShareList(_ *types.UserShareListRequest) (resp *types.UserShareListReply, err error) {
-	// todo: add your logic here and delete this line
+	shareFile := make([]*types.ShareBasicDetailReply, 0)
+	resp = new(types.UserShareListReply)
 
+	err = l.svcCtx.Engine.
+		Table("share_basic").
+		Select("share_basic.identity, share_basic.repository_identity, user_repository.name, repository_pool.ext, "+
+			"repository_pool.path, repository_pool.size, share_basic.click_num, share_basic.desc, "+
+			"user_basic.name as owner, user_basic.avatar, share_basic.expired_time, share_basic.updated_at").
+		Joins("LEFT JOIN repository_pool ON repository_pool.identity = share_basic.repository_identity").
+		Joins("LEFT JOIN user_repository ON user_repository.identity = share_basic.user_repository_identity").
+		Joins("left join user_basic on share_basic.user_identity = user_basic.identity").
+		Where("share_basic.click_num > ?", 10).
+		Where("share_basic.deleted_at IS NULL").
+		Order("share_basic.click_num desc").
+		Find(&shareFile).Error
+
+	if err != nil {
+		return
+	}
+
+	resp.List = shareFile
 	return
+
 }
